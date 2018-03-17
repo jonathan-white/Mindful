19cee41aad44cf2b1db92fa83f6730b675edc8b6
MotionSwing <jon.white2@gmail.com>
2018-03-17 14:02:50 -0400

    commit

--- assets/js/news.js
+++ assets/js/news.js
@@ -16,17 +16,38 @@ $.ajax({
 }).then(function(response) {
 	console.log(response);
 	var articles = response.articles;
+	var col_one = $(".img-col-1");
+	var col_two = $(".img-col-2");
+	var col_three = $(".img-col-3");
 
-	for (var i = 0; i < articles.length; i++) {
-		$("#news-container").append(generateCard(articles[i]));
+	for (var c = 0; c < 3; c++) {
+		if (c === 0){
+			for (var r = 0; r < 7; r++) {
+				generateCard(articles[r], col_one);
+			}
+		}else if (c === 1){
+			for (var r = 8; r < 15; r++) {
+				generateCard(articles[r], col_two);
+			}
+		}else if(c === 2){
+			for (var r = 16; r < 20; r++) {
+				generateCard(articles[r], col_three);
+			}	
+		}
 	}
+
+	// for (var i = 0; i < articles.length; i++) {
+	// 	$("#news-container").append(generateCard(articles[i]));
+	// }
+
+
 
 	// generateCard(articles[i]);
 
 });
 
-function generateCard(arrItem){
-	var card_wrapper = $("<div class='col-3'>");
+function generateCard(arrItem, target){
+	var card_wrapper = $("<div>");
 
 	var card_body_link = $("<a>").attr({
 		href: arrItem.url
@@ -49,7 +70,7 @@ function generateCard(arrItem){
 	card_body.append(card_body_h5,card_body_p);
 	card_body_link.append(card);
 	card_wrapper.append(card_body_link);
-	$("#news-container").append(card_wrapper);
+	target.append(card_wrapper);
 }
 
 // function 
