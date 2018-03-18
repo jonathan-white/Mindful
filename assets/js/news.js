ef1aad5b887fd7739475fb2809ebe4703615dc66
MotionSwing <jon.white2@gmail.com>
2018-03-17 22:14:11 -0400

    JW commit

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,87 +1,82 @@
-//e95105e255ab4ca5b069b303112caa05
+$(document).ready(function(){
+	// ------------------------------
+	// News API Endpoint & Ajax Call
+	// ------------------------------
+	// Setup endpoint definition
+	var endpoint = 'https://newsapi.org/v2/top-headlines?';
+	endpoint += 'country=us';
+	endpoint += '&apiKey=e95105e255ab4ca5b069b303112caa05';
+	// endpoint += "&pageSize=40";
 
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=e95105e255ab4ca5b069b303112caa05';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+	$.ajax({
+		url: endpoint,
+		type: 'GET'
+	}).then(function(response) {
+		// Console request reponse
+		console.log(response);
 
+		// An array to hold the returned list of articles
+		var articles = response.articles;
 
-$.ajax({
-	url: url,
-	type: 'GET'
-}).then(function(response) {
-	console.log(response);
-	var articles = response.articles;
-	var col_one = $(".img-col-1");
-	var col_two = $(".img-col-2");
-	var col_three = $(".img-col-3");
+		// Variables to reference the 3 columns where the articles will be placed
+		var col_one = $(".img-col-1");
+		var col_two = $(".img-col-2");
+		var col_three = $(".img-col-3");
 
-	for (var c = 0; c < 3; c++) {
-		if (c === 0){
-			for (var r = 0; r < 7; r++) {
-				generateCard(articles[r], col_one);
+		// For each of the three columns, generate 7 article cards from the articles array
+		var newCard;
+		for (var c = 0; c < 3; c++) {
+			if (c === 0){
+				for (var r = 0; r < 7; r++) {
+					newCard = aCard(articles[r]);
+					// newCard.animate({
+					// 	bottom: "120%"
+					// 	},750, function() {
+
+					// });
+					// newCard.css('animation-delay', "800ms");
+					col_one.append(newCard);
+				}
+			}else if (c === 1){
+				for (var r = 7; r < 14; r++) {
+					col_two.append(aCard(articles[r]));
+				}
+			}else if(c === 2){
+				for (var r = 14; r < 20; r++) {
+					col_three.append(aCard(articles[r]));
+				}	
 			}
-		}else if (c === 1){
-			for (var r = 8; r < 15; r++) {
-				generateCard(articles[r], col_two);
-			}
-		}else if(c === 2){
-			for (var r = 16; r < 20; r++) {
-				generateCard(articles[r], col_three);
-			}	
 		}
+	});
+
+	// Purpose: Generates a bootstrap card in the DOM. 
+	// Parameter(s): 
+	// * article: a single article entry from the NewsAPI response
+	function aCard(article){
+		var card;
+		// Ensure the article entry has an image available to display.
+		if(article.urlToImage != null && article.urlToImage.startsWith("http")) {
+			card = $("<div class='card'>");
+			// Image tag and pull image source from article entry
+			const img = $("<img class='card-img-top'>").attr({
+				src: article.urlToImage,
+				alt: article.title
+			});
+			// Text overlay wrapper div
+			const card_body = $("<div class='card-body'>");
+
+			// Article Source
+			const card_body_h5 = $("<h5 class='card-title'>").text(article.title);
+
+			// Article Title (brief description)
+			const card_body_p = $("<p class='card-text'>").text("Source: " + article.source.name);
+
+			// Add elements to each other and then to the DOM
+			card_body.append(card_body_h5,card_body_p);
+			card.append(img,card_body);
+			// target.append(card);
+		}
+		return card;
 	}
-
-	// for (var i = 0; i < articles.length; i++) {
-	// 	$("#news-container").append(generateCard(articles[i]));
-	// }
-
-
-
-	// generateCard(articles[i]);
 
 });
-
-function generateCard(arrItem, target){
-	var card_wrapper = $("<div>");
-
-	var card_body_link = $("<a>").attr({
-		href: arrItem.url
-	}).removeClass('btn btn-primary');
-
-	var card = $("<div class='card'>");
-
-	if(arrItem.urlToImage != null) {
-		var img = $("<img class='card-img-top'>").attr({
-			src: arrItem.urlToImage,
-			alt: "Image"
-		});
-		card.append(img);
-	}
-	var card_body = $("<div class='card-body'>");
-	var card_body_h5 = $("<h5 class='card-title'>").text("Source: " + arrItem.source.name);
-	var card_body_p = $("<p class='card-text'>").text(arrItem.title);
-
-	card.append(card_body);
-	card_body.append(card_body_h5,card_body_p);
-	card_body_link.append(card);
-	card_wrapper.append(card_body_link);
-	target.append(card_wrapper);
-}
-
-// function 
-
-// 		<div class="col-3">
-// 			<div class="card">
-// 			  <img class="card-img-top" src="..." alt="Card image cap">
-// 			  <div class="card-body">
-// 			    <h5 class="card-title">Card title</h5>
-// 			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-// 			    <a href="#" class="btn btn-primary">Go somewhere</a>
-// 			  </div>
-// 			</div>
-// 		</div>
