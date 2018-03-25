b733ea1c583a2f11ac69f517ec1e2bffce4e0eba
MotionSwing <jon.white2@gmail.com>
2018-03-25 16:07:16 -0400

    updated mindfill page

--- assets/js/mindfill.js
+++ assets/js/mindfill.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	var database = firebase.database();
 
 	var userID;
+
+	// Only display items if the user is logged in
 	if(localStorage.userID){
 		userID = localStorage.getItem("userID");
 		// Use this codeblock to post saved books/news articles to user's mindfill 
@@ -10,7 +12,7 @@ $(document).ready(function(){
 		// Display Viewed News Articles
 		database.ref('users/' + userID + '/news').on('child_added', function(childSnapshot) {
 			console.log(childSnapshot.key);
-			var card = $("<div class='card' style='width: 18rem;'>");
+			var card = $("<div class='card mb-3'>");
 			var card_img = $("<img class='card-img-top'>");
 			card_img.attr({
 				'src': childSnapshot.val().newsRef.urlToImage,
@@ -18,7 +20,7 @@ $(document).ready(function(){
 			});
 			var card_body = $("<div class='card-body'>");
 			var card_title = $("<h5 class='card-title'>").text(childSnapshot.val().newsRef.title);
-			var card_text = $("<p class='card-text'>").text(childSnapshot.val().newsRef.description);
+			var card_text = $("<p class='card-text'>").text('');
 			var card_link = $("<a class='btn btn-primary' target='_blank'>").text("Go to Article");
 			card_link.attr('href', childSnapshot.val().newsRef.url);
 			card_body.append(card_title, card_text, card_link);
@@ -30,7 +32,7 @@ $(document).ready(function(){
 		// Display Viewed Books
 		database.ref('users/' + userID + '/books').on('child_added', function(childSnapshot) {
 			console.log(childSnapshot.key);
-			var card = $("<div class='card' style='width: 18rem;'>");
+			var card = $("<div class='card mb-3'>");
 			var card_img = $("<img class='card-img-top'>");
 			card_img.attr({
 				'src': childSnapshot.val().coverURL,
@@ -47,15 +49,4 @@ $(document).ready(function(){
 		});
 	}
 
-
-
 });
-
-// <div class="card" style="width: 18rem;">
-//   <img class="card-img-top" src="..." alt="Card image cap">
-//   <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//     <a href="#" class="btn btn-primary">Go somewhere</a>
-//   </div>
-// </div>
